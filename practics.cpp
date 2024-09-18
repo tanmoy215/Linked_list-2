8b2ffc6ca162aa14d99455e911107a837968819c
Tanmoy <majitanmoy14@gmail.com>
2024-09-18 10:07:34 +0530

    sep 18

--- practics.cpp
+++ practics.cpp
@@ -4,49 +4,53 @@ class Node{
    public:
     int data;
     Node* next;
-    Node(int n){
-        data = n;
-        next=NULL;
+    Node(int data){
+       this->data = data;
+       this->next = nullptr;
     }
 };
-Node* remove(Node* head,int idx){
-     Node* slow = head;
-     Node* fast = head;
-      for(int i=1;i<=idx+1;i++){
-        if(fast==NULL) return head->next;
-        fast = fast->next;
-      }
-      while(fast!=NULL){
-        slow = slow->next;
-        fast = fast->next;
-      }
-      slow->next = slow->next->next;
-      return head;
-}
 void display(Node* head){
+  while(head!=nullptr){
+     cout<<head->data<<" ";
+     head = head->next;
+  }
+  cout<<endl;
+}
+int mmiddle(Node* head){
+   int size=0;
    Node* temp = head;
-   while(temp!=NULL){
-    cout<<temp->data<<" ";
-    temp = temp->next;
+    while(temp!=NULL){
+      size++;
+      temp = temp->next;    //using size
+    }
+    Node* help = head;
+   int mid = size/2;
+   for(int i=1;i<=mid;i++){
+      help = help->next;
    }
-   cout<<endl;
+   return help->data;
+}
+void middle(Node* head){
+   Node* slow = head;
+   Node* fast = head;
+   while(fast->next->next!=nullptr && fast->next->next->next!=nullptr){
+     slow=slow->next;
+     fast = fast->next->next;
+   }
+   slow->next = slow->next->next;
 }
 int main(){
-    Node* head = new Node(10);
-    Node* a = new Node(10);
-    Node* b = new Node(50);
-    Node* c = new Node(40);
-    Node* d = new Node(60);
-    Node* e = new Node(90);
-    Node* tail = new Node(170);
-    head->next = a;
-    a->next = b;
-    b->next = c;
-    c->next = d;
-    d->next = e;
-    e->next =tail;
-     display(head);
-     head = remove(head,2);
-     display(head);
-    return 0;
+   Node* head = new Node(10);
+   Node* a = new Node(20);
+   Node* b = new Node(30);
+   Node* c = new Node(40);
+   Node* d = new Node(50);
+   head->next =a;
+   a->next = b;
+   b->next = c;
+   c->next = d;
+   display(head);
+   middle(head);
+   display(head);
+  return 0;
 }
